6d94c6fbefc446a8252eb0117c0e166b08153060
tanSergo <tan_sergo@mail.ru>
2016-06-04 22:54:45 +0400

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,7 +1,11 @@
 node default {
-  java::oracle { 'jdk8' :
-    ensure  => 'present',
-    version => '8',
-    java_se => 'jdk',
-  }
+  #java::oracle { 'jdk8' :
+  # ensure  => 'present',
+  # version => '8',
+  #  java_se => 'jdk',
+  #}
+  class { 'oracle_java':
+  version => '8u45',
+  type    => 'jdk'
+}
 }
